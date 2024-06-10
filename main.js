f38e6621bd5130f6fc37323b6c42329ea64da37a
Joao Paiva <paivaseven@gmail.com>
2024-06-09 23:45:56 -0300

    recriando os arquivos apos reseta-los

---
+++ main.js
@@ -0,0 +1,32 @@
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.querySelector('#subtitulo');
+typeWrite(titulo);
+
+window.addEventListener('DOMContentLoaded', function (){
+
+   let formulario = document.getElementsByClassName("formulario")[0]
+
+   formulario.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    let obj = {nome: e.target[0].value, email: e.target[1].value, mensagem: e.target[2].value}
+    console.log(obj);
+    setTimeout(()=>{
+        alert('Agradeço o contato, entrarei em contato assim que puder');
+        window.location.href = "#";
+        document.getElementById("nome").value = '';
+        document.getElementById("email").value = '';
+        document.getElementById("mensagem").value = '';
+
+    },3000)
+   });
+})
